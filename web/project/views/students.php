e986e887150b9778a55329bf0b17ca63de22977e
mhvsoftware <creaproyecto@hotmail.com>
2020-10-17 16:50:10 -0500

    project part 13

--- web/project/views/students.php
+++ web/project/views/students.php
@@ -5,9 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Students</title>
     <main>
-    <?php
-     echo($displayStudentsInfo)
-    ?>
+    <table>
+        <?php
+        echo($displayStudentsInfo)
+        ?>
+    </table>
     </main>
 </head>
 <body>

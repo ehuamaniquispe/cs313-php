f56c6b21f2394b7f17bf001aef919688b519c86b
mhvsoftware <creaproyecto@hotmail.com>
2020-10-17 16:41:57 -0500

    project part 13

--- web/project/students/index.php
+++ web/project/students/index.php
@@ -7,54 +7,49 @@
 // // echo("Requiered authorization");
 // // die();
 
-// // require_once '../library/connection.php';
-// // require_once '../model/students-model.php';
-// // require_once '../library/functions.php';
+require_once '../library/connection.php';
+require_once '../model/students-model.php';
+require_once '../library/functions.php';
 
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL){
-    echo("GET");
     $action = $_GET['action'];
-    echo($action);
-}
-else{
-
-    echo("POST");
 }
 
-// switch ($action){
+
+switch ($action){
 
 
-//     case 'checkCredentials':
-//     //  $userName = filter_input(INPUT_POST,'userName', FILTER_SANITIZE_STRING);
-//     //  $pass = filter_input(INPUT_POST,'pass', FILTER_SANITIZE_STRING);
+    case 'checkCredentials':
+    //  $userName = filter_input(INPUT_POST,'userName', FILTER_SANITIZE_STRING);
+    //  $pass = filter_input(INPUT_POST,'pass', FILTER_SANITIZE_STRING);
 
-//     //  $checkUser = checkUser($userName,$pass);
+    //  $checkUser = checkUser($userName,$pass);
 
-//     //  if(empty($checkUser)){
-//     //      echo("User not found");
-//     //  }
-//     //  else{
-//     //      $_SESSION['userName'] = $userName;
-//     //      $_SESSION['userLevel'] = $checkUser[user_level]; 
-//     //     //   echo ($_SESSION['userLevel']);
-//     //     header('location:../students'); 
-//     //     // include'../students'; 
+    //  if(empty($checkUser)){
+    //      echo("User not found");
+    //  }
+    //  else{
+    //      $_SESSION['userName'] = $userName;
+    //      $_SESSION['userLevel'] = $checkUser[user_level]; 
+    //     //   echo ($_SESSION['userLevel']);
+    //     header('location:../students'); 
+    //     // include'../students'; 
 
-//     //  }
-//     //  print_r($checkUser);
+    //  }
+    //  print_r($checkUser);
 
-//     // //  echo ($pass);
+    // //  echo ($pass);
 
-//     break;
+    break;
 
-//     default:
+    default:
     
-//     $getStudentsInfo=getStudentsInfo();
-//     $displayStudentsInfo = displayStudentsInfo($getStudentsInfo);
+    $getStudentsInfo=getStudentsInfo();
+    $displayStudentsInfo = displayStudentsInfo($getStudentsInfo);
 
-//     include '../view/students.php';
-//     echo("default");
+    include '../view/students.php';
+    echo("default");
 
-// }
+}
 

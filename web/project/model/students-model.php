14bad0ce45435fb408251de480e5e31f694f2e7a
mhvsoftware <creaproyecto@hotmail.com>
2020-10-23 18:57:00 -0500

    team week 5

--- web/project/model/students-model.php
+++ web/project/model/students-model.php
@@ -27,18 +27,32 @@ $sql = "INSERT INTO students
         students_user_pass,
         students_user_level)
         VALUES(
-        :student_reg_num,
-        :student_fname,
-        :student_lname,
-        :student_user_name,
-        :student_user_pass,
+        '5',
+        'matias',
+        'huamani',
+        'mhuamani',
+        'mhuamani',
         DEFAULT)";
+// $sql = "INSERT INTO students
+//         (students_reg_num,
+//         students_fname,
+//         students_lname,
+//         students_user_name,
+//         students_user_pass,
+//         students_user_level)
+//         VALUES(
+//         :student_reg_num,
+//         :student_fname,
+//         :student_lname,
+//         :student_user_name,
+//         :student_user_pass,
+//         DEFAULT)";
 $stmt = $bd->prepare($sql);
-$stmt->bindValue(':student_reg_num',$student_reg_num,PDO::PARAM_STR);
-$stmt->bindValue(':student_fname',$student_fname,PDO::PARAM_STR);
-$stmt->bindValue(':student_lname',$student_lname,PDO::PARAM_STR);
-$stmt->bindValue(':student_user_name',$student_user_name,PDO::PARAM_STR);
-$stmt->bindValue(':student_user_pass',$student_user_pass,PDO::PARAM_STR);
+// $stmt->bindValue(':student_reg_num',$student_reg_num,PDO::PARAM_STR);
+// $stmt->bindValue(':student_fname',$student_fname,PDO::PARAM_STR);
+// $stmt->bindValue(':student_lname',$student_lname,PDO::PARAM_STR);
+// $stmt->bindValue(':student_user_name',$student_user_name,PDO::PARAM_STR);
+// $stmt->bindValue(':student_user_pass',$student_user_pass,PDO::PARAM_STR);
 $stmt->execute();
 $rowChanged=$stmt->rowCount();
 $stmt->closeCursor();

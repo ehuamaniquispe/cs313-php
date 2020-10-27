38d6cdb1a71624a3640a45757a3561248c587f39
mhvsoftware <creaproyecto@hotmail.com>
2020-10-27 18:50:12 -0500

    project

--- web/project/js/functions.js
+++ web/project/js/functions.js
@@ -3,5 +3,7 @@ $(document).ready(function(){
 
     $(".course-view").click(function(){
         console.log("me clikeo");
+        let courseId = this.attr("course-id");
+        console.log(courseId);
     })
 })
--- web/project/library/functions.php
+++ web/project/library/functions.php
@@ -65,7 +65,7 @@ function displayCoursesInfo($courses)
         $tableCourses .= "<td> $course[courses_name] </td>";
         $tableCourses .= "<td> $course[courses_section] </td>";
        
-        $tableCourses .= "<td> <a class='course-view' title='view'> VIEW </a> <a href='../courses/index.php?action=edit&course_id=$course[id_courses]' title='edit'> EDIT </a> <a href='../courses/index.php?action=delete&course_id=$course[id_courses]' 'title='delete'> DELETE </a></td>";
+        $tableCourses .= "<td> <a class='course-view' course_id='$course[id_courses]' title='view'> VIEW </a> <a href='../courses/index.php?action=edit&course_id=$course[id_courses]' title='edit'> EDIT </a> <a href='../courses/index.php?action=delete&course_id=$course[id_courses]' 'title='delete'> DELETE </a></td>";
         // $tableCourses .= "<td> <a href='../courses/index.php?action=viewDetail&courseId=$course[id_courses]' title='view'> VIEW </a> <a href='../courses/index.php?action=edit&course_id=$course[id_courses]' title='edit'> EDIT </a> <a href='../courses/index.php?action=delete&course_id=$course[id_courses]' 'title='delete'> DELETE </a></td>";
       
         $tableCourses .= '</tr>';

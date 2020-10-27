fda51df6da89dca97c36c97108d5e039d70ebf94
mhvsoftware <creaproyecto@hotmail.com>
2020-10-27 18:55:42 -0500

    project

--- web/project/js/functions.js
+++ web/project/js/functions.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
     $(".course-view").click(function(){
         console.log("me clikeo");
-        let courseId = $(this).attr("course-id");
+        let courseId = $(this).attr("course_id");
         console.log(courseId);
     })
 })
